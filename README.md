7f612f7d0c77eb4300e681c353895516158f2c3c
= <=>
2026-02-07 11:26:28 -0500

    new

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # nube
+#
+# * levantan la api:
+# uvicorn main:app  --host 0.0.0.0 --reload
+#
+# * con postman/bruno o desde cmd (curl o invoke) a la ip: http://localhost:8000/predict
+#
+# con metodo post y este cuerpo en el json:
+#
+# "edad": 50,
+# "clase": "Third",
+# "sexo" : "male"
+#
+# docker run -p 8000:8000 3214944867/api-model:1.0
+# docker login
+# docker push 3214944867/api-model:1.0
+# 
--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
+import numpy as np 
 import pandas as pd
-import numpy as np
+import pickle
+import os
 
 
 app = FastAPI()
 
-class Prediction(BaseModel):
+# edad Usuario 
+
+class Prediccion(BaseModel):
     edad: int
     clase: str
     sexo: str
+
+# Modelo de sobrevivientes del Titanic
+
+# Obtener la ruta absoluta del archivo del modelo
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'titanic_model.pkl')
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+@app.post("/predict")
+def predict_survival(data: Prediccion):
+    # Convertir datos de entrada a DataFrame
+    input_data = pd.DataFrame([data.dict()])
+
+    # Preprocesamiento 
+    input_data['sexo'] = input_data['sexo'].map({'M': 0, 'F': 1})
+    input_data['clase'] = input_data['clase'].map({'First': 1, 'Second': 2, 'Third': 3})
+    input_data = input_data.fillna(input_data.mean())
+    features = input_data[['edad', 'clase', 'sexo']]
+    prediction = model.predict(features)
+    survival = 'De buenas' if prediction[0] == 1 else 'Pailas se muere'
+    return {"nivel de salades": survival}
+
 @app.get("/test")
 def testApi():
-    return {"message": "viva mexico"}
+    return {"message": "API is working"}
